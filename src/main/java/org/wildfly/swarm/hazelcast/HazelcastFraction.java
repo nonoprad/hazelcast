dca971db1b37a81032d7eb99ddb8f0cd4bccb680
nonoprad <nonoprad@free.fr>
2018-03-07 19:43:25 +0100

    first commit

---
+++ src/main/java/org/wildfly/swarm/hazelcast/HazelcastFraction.java
@@ -0,0 +1,15 @@
+package org.wildfly.swarm.hazelcast;
+
+import org.wildfly.swarm.spi.api.Fraction;
+
+public class HazelcastFraction implements Fraction<HazelcastFraction> {
+
+
+    private String[] hostnamesWithPorts;
+
+    public HazelcastFraction(){
+
+    }
+
+
+}
---
+++ src/main/java/org/wildfly/swarm/hazelcast/HazelcastProperties.java
@@ -0,0 +1,9 @@
+package org.wildfly.swarm.hazelcast;
+
+import java.util.List;
+
+public class HazelcastProperties {
+
+
+
+}
